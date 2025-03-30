4292b285366666b205a1c6915b6b78ee7c920275
이승엽 <sylee@spectra.co.kr>
2025-03-30 19:38:30 +0900

    - 과제3 진행  > RAG 수행

---
+++ 99_projects/03_embedding.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+from langchain_community.document_loaders import PDFPlumberLoader
+from langchain_elasticsearch import ElasticsearchStore
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+load_dotenv()
+
+
+def run_embedding():
+    data_file = "./data/short_selling_invest.pdf"
+    # data_file = "./data/2024_프로야구_리그규정_요약.pdf"
+
+    loader = PDFPlumberLoader(file_path=data_file)
+    docs = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=300, chunk_overlap=50)
+    split_docs = text_splitter.split_documents(docs)
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+    vector_store = ElasticsearchStore(
+        es_url="http://172.16.120.203:9201",
+        index_name="sylee_pdf",
+        embedding=embeddings,
+    )
+
+    vector_store.add_documents(split_docs)
+
+
+if __name__ == "__main__":
+    run_embedding()
